9b9a2bce5bf461e2f614dc6191d8eaedb299d425
时间旅人 <13285454+APERTURE_Lab@user.noreply.gitee.com>
2025-03-31 10:00:22 +0800

    message 7: Refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -6,14 +6,6 @@ from django.http import HttpRequest
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_return_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
